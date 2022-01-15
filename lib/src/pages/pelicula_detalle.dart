67856741a78685f88003027bbf8de585748d5a1e
Jorge Hernandez <hernandezrayojorge@gmail.com>
2022-01-14 22:08:11 -0500

    validacion de floating button

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -6,7 +6,6 @@ import 'package:peliculas/src/providers/peliculas_providers.dart';
 import 'package:share/share.dart';
 
 class PeliculaDetalle extends StatelessWidget {
-
   final peliculasProvider = new PeliculasProvider();
 
   @override
@@ -15,19 +14,18 @@ class PeliculaDetalle extends StatelessWidget {
 
     return Scaffold(
         body: CustomScrollView(
-      slivers: [
-        _crearAppbar(pelicula),
-        SliverList(
-            delegate: SliverChildListDelegate([
-          SizedBox(height: 10.0),
-          _posterTitulo(context, pelicula),
-          _descripcion(pelicula),
-          _crearCasting(pelicula),
-        ]))
-      ],
-    ),
-    floatingActionButton: _buttonFloating(pelicula.id.toString())
-    );
+          slivers: [
+            _crearAppbar(pelicula),
+            SliverList(
+                delegate: SliverChildListDelegate([
+              SizedBox(height: 10.0),
+              _posterTitulo(context, pelicula),
+              _descripcion(pelicula),
+              _crearCasting(pelicula),
+            ]))
+          ],
+        ),
+        floatingActionButton: _buttonFloating(pelicula.id.toString()));
   }
 
   Widget _crearAppbar(Pelicula pelicula) {
@@ -65,25 +63,31 @@ class PeliculaDetalle extends StatelessWidget {
                 child: Image(
                   image: NetworkImage(pelicula.getPosterImg()),
                   height: 150.0,
-                )
-            ),
+                )),
           ),
           SizedBox(width: 20.0),
           Flexible(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(pelicula.title, style: Theme.of(context).textTheme.bodyText1, overflow: TextOverflow.ellipsis),
-                Text(pelicula.originalTitle, style: Theme.of(context).textTheme.subtitle1, overflow: TextOverflow.ellipsis),
-                Row(
-                  children: [
-                    Icon(Icons.star, color: Colors.orange,),
-                    Text(pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subtitle1)
-                  ],
-                )
-              ],
-            )
-          )
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(pelicula.title,
+                  style: Theme.of(context).textTheme.bodyText1,
+                  overflow: TextOverflow.ellipsis),
+              Text(pelicula.originalTitle,
+                  style: Theme.of(context).textTheme.subtitle1,
+                  overflow: TextOverflow.ellipsis),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.orange,
+                  ),
+                  Text(pelicula.voteAverage.toString(),
+                      style: Theme.of(context).textTheme.subtitle1)
+                ],
+              )
+            ],
+          ))
         ],
       ),
     );
@@ -92,11 +96,13 @@ class PeliculaDetalle extends StatelessWidget {
   Widget _descripcion(Pelicula pelicula) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-        child: Text(pelicula.overview,
+      child: Text(
+        pelicula.overview,
         textAlign: TextAlign.justify,
       ),
     );
   }
+
   Widget _crearCasting(Pelicula pelicula) {
     final peliProvider = PeliculasProvider();
     return FutureBuilder(
@@ -116,10 +122,10 @@ class PeliculaDetalle extends StatelessWidget {
     return FutureBuilder(
       future: peliProvider.getVideos(pelicula),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasData) {
+        if (snapshot.hasData && snapshot.data.length != 0) {
           return _floatingButton(context, snapshot.data[0]);
         } else {
-          return Center(child: CircularProgressIndicator());
+          return Visibility(visible: false, child: Text(''));
         }
       },
     );
@@ -133,26 +139,19 @@ class PeliculaDetalle extends StatelessWidget {
       backgroundColor: Colors.redAccent,
       onPressed: () {
         final RenderBox box = context.findRenderObject();
-        Share.share(
-          peli.getUrlVideo(),
-          subject: subject,
-          sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size
-        );
+        Share.share(peli.getUrlVideo(),
+            subject: subject,
+            sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
       },
     );
   }
 
-
   Widget _crearActoresPageView(List<Actor> actores) {
-    
     return SizedBox(
       height: 200.0,
       child: PageView.builder(
         pageSnapping: false,
-        controller: PageController(
-          viewportFraction: 0.3,
-          initialPage: 1
-        ),
+        controller: PageController(viewportFraction: 0.3, initialPage: 1),
         itemCount: actores.length,
         itemBuilder: (context, i) => _actorTarjeta(actores[i]),
       ),
@@ -164,8 +163,8 @@ class PeliculaDetalle extends StatelessWidget {
       child: Column(
         children: [
           ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: FadeInImage(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
               image: NetworkImage(actor.getFoto()),
               placeholder: AssetImage('assets/img/no-image.jpg'),
               height: 150.0,
